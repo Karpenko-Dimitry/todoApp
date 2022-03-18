b01720ca6eb33fcfd62d0cf64ec50bedd2eb37be
Karpenko Dimitry <karpenko.dimitry@gmail.com>
2022-03-18 13:20:14 +0200

    added icons

--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-eva-icons';
 
 export default function TodoItem({ item, pressHandler }) {
     return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
-        </TouchableOpacity>
+        
+            <View style={styles.item}>
+                <TouchableOpacity>
+                <Icon
+                    name="close-outline"
+                    fill="#bbb"
+                    width={20}
+                    height={20}
+                    style={styles.icon}
+                    onPress={() => pressHandler(item.key)}
+                />
+                 </TouchableOpacity>
+                <Text>{item.text}</Text>
+            </View>
+       
     );
 }
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
+        alignItems: 'center',
         padding: 16,
         marginTop: 16,
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
         borderRadius: 10,
+    },
+    icon: {
+        marginRight: 10,
     },
 });
